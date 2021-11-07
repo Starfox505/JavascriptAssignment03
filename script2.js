02f41ff98ac7e784f378411a81ab2c17aa57a8f4
Brendan Drewry <superbrendan715@yahoo.com>
2021-11-07 11:55:17 -0800

    Update script2.js
    Updated to try and fix nesting and add else statement, but it is still not working correctly.

--- script2.js
+++ script2.js
@@ -13,10 +13,12 @@
 let maxNumber = 100;
 
 for (let i = 1; i <= maxNumber; i++)
-    if (i % 5 == 0){
-        console.log("Buzz");
-            if (i % 3 == 0)
-            console.log("Fizz");
+    if (i % 3 == 0){
+        console.log("Fizz");
+            if (i % 5 == 0)
+            console.log("Buzz");
             if (i % 3 == 0 && i % 5 == 0);
             console.log("FizzBuzz");
+        } else {
+            console.log(i);
         }
