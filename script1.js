c6cc3ed34909bfbb88b0ecdf5e708e93c557bb1c
Brendan Drewry <superbrendan715@yahoo.com>
2021-11-08 20:25:45 -0800

    Update script1.js
    Finally figured out ascending and descending loop! Finished javascript 1

--- script1.js
+++ script1.js
@@ -21,15 +21,18 @@ while (j >= 0){
 }
 */
 
-let outputString = "";
-for (let j = 1; j <= 4; j++)
+for (let j = 1; j <= 4; j++) {
+  let outputString = "";
   for (let i = 1; i <= j; i++) {
     outputString = outputString + "#";
   }
-console.log(outputString);
+  console.log(outputString);
+}
 
-for (let j = 1; j <= 4; j--)
-  for (let i = 1; i <= j; i--) {
+for (let j = 3; j >= 1; j--) {
+  let outputString = "";
+  for (let i = 1; i <= j; i++) {
     outputString = outputString + "#";
   }
-console.log(outputString);
+  console.log(outputString);
+}
