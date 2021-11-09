0dc0049ff5b08475c60599278faaeaf325e04505
Brendan Drewry <superbrendan715@yahoo.com>
2021-11-08 19:23:57 -0800

    Update script2.js
    Fixed nesting, as the order was all off. Finally got it right and working!

--- script2.js
+++ script2.js
@@ -12,15 +12,14 @@
 
 let maxNumber = 100;
 
-for (let i = 1; i <= maxNumber; i++)
-    if (i % 3 == 0){
-        console.log("Fizz");
-            if (i % 5 == 0){
-            console.log("Buzz");
-            }
-            if (i % 3 == 0 && i % 5 == 0){
-            console.log("FizzBuzz");
-            }    
-        } else {
-            console.log(i);
-        }
+for (let i = 1; i <= maxNumber; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
